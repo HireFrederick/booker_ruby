f49a2791882c8272d4ec5acb9c4789ecec3cbc7c
Daniel Greenberg <daniel@hirefrederick.com>
2016-04-08 17:00:15 -0700

    add names to code functionality to country

--- lib/booker/models/country.rb
+++ lib/booker/models/country.rb
@@ -1,5 +1,9 @@
 module Booker
   module Models
-    class Country < Type; end
+    class Country < Type
+      NAMES_TO_CODES = YAML::load_file(File.join(__dir__, '..', 'config', 'booker_countries.yml')).freeze
+
+      def country_code; NAMES_TO_CODES[self.Name]; end
+    end
   end
 end
--- spec/lib/booker/models/country_spec.rb
+++ spec/lib/booker/models/country_spec.rb
@@ -2,4 +2,21 @@ require 'spec_helper'
 
 describe Booker::Models::Country do
   it { is_expected.to be_a(Booker::Models::Type) }
+
+  describe 'constants' do
+    let(:names_to_codes) { described_class::NAMES_TO_CODES }
+
+    it 'sets constants to right vals' do
+      expect(names_to_codes).to be_a Hash
+      expect(names_to_codes.length).to eq 268
+    end
+  end
+
+  describe '#country_code' do
+    let(:country_name) { "Cote D'Ivoire (Ivory Coast)" }
+
+    subject { described_class.new('Name' => country_name) }
+
+    it('returns right country code for name') { expect(subject.country_code).to eq 'CI' }
+  end
 end
