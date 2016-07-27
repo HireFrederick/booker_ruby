c1bdc79513d6ebdbfef3db8f5ec2bae95ff9ad5e
Aaron Severs <aaron@hirefrederick.com>
2016-07-26 17:12:03 -0700

    map booker country ids to codes directly instead of relying on name

--- lib/booker/models/country.rb
+++ lib/booker/models/country.rb
@@ -2,9 +2,9 @@ module Booker
   module Models
     class Country < Type
       # ISO 3166-1 alpha-2 Codes
-      NAMES_TO_ISO_CODES = YAML::load_file(File.join(__dir__, '..', 'config', 'booker_country_names_to_iso_codes.yml')).freeze
+      IDS_TO_ISO_CODES = YAML::load_file(File.join(__dir__, '..', 'config', 'booker_country_ids_to_iso_codes.yml')).freeze
 
-      def country_code; NAMES_TO_ISO_CODES[self.Name]; end
+      def country_code; IDS_TO_ISO_CODES[self.ID]; end
     end
   end
 end
--- spec/lib/booker/models/country_spec.rb
+++ spec/lib/booker/models/country_spec.rb
@@ -4,24 +4,23 @@ describe Booker::Models::Country do
   it { is_expected.to be_a(Booker::Models::Type) }
 
   describe 'constants' do
-    let(:names_to_codes) { described_class::NAMES_TO_ISO_CODES }
+    let(:ids_to_codes) { described_class::IDS_TO_ISO_CODES }
 
     it 'sets constants to right vals' do
-      expect(names_to_codes).to be_a Hash
-      expect(names_to_codes.length).to eq 267
-      names_to_codes.values.each do |country_code|
+      expect(ids_to_codes).to be_a Hash
+      expect(ids_to_codes.length).to eq 268
+      ids_to_codes.values.each do |country_code|
         expect(country_code.class).to eq String
         expect(country_code.length).to eq 2
-        expect(Carmen::Country.coded(country_code)).to_not eq nil
       end
     end
   end
 
   describe '#country_code' do
-    let(:country_name) { "Cote D'Ivoire (Ivory Coast)" }
+    let(:country_id) { 244 }
 
-    subject { described_class.new('Name' => country_name) }
+    subject { described_class.new('ID' => country_id) }
 
-    it('returns right country code for name') { expect(subject.country_code).to eq 'CI' }
+    it('returns right country code for name') { expect(subject.country_code).to eq 'TC' }
   end
 end
