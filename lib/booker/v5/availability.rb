aff3ca027368a69613fb36ab65e5f310860bcd2d
Aaron Severs <aaron@hirefrederick.com>
2017-06-16 14:00:07 -0700

    remove invalid v5 availability methods

--- lib/booker/v5/availability.rb
+++ lib/booker/v5/availability.rb
@@ -3,7 +3,6 @@ module Booker
     class Availability < Booker::Client
       API_METHODS = {
         availability: '/v5/availability/availability'.freeze,
-        one_day_availability: '/v5/availability/1day',
         two_day_availability: '/v5/availability/2day'.freeze,
         thirty_day_availability: '/v5/availability/30day'.freeze
       }.freeze
@@ -13,14 +12,6 @@ module Booker
           locationIds: location_ids,
           fromDateTime: from_date_time,
           toDateTime: to_date_time,
-          includeEmployees: include_employees
-        }, Booker::V5::Models::AvailabilityResult
-      end
-
-      def one_day(location_id:, from_date_time:, include_employees: true)
-        get API_METHODS[:one_day_availability], {
-          locationId: location_id,
-          fromDateTime: from_date_time,
           includeEmployees: include_employees
         }, Booker::V5::Models::AvailabilityResult
       end
--- spec/lib/booker/v5/availability_spec.rb
+++ spec/lib/booker/v5/availability_spec.rb
@@ -28,29 +28,6 @@ describe Booker::V5::Availability do
     end
   end
 
-  describe '#one_day' do
-    let(:location_id) { 456 }
-    let(:from_date_time) { 'from_date_time' }
-    let(:to_date_time) { 'to_date_time' }
-    let(:result) { client.one_day(location_id: location_id, from_date_time: from_date_time) }
-    let(:expected_params) {{
-      locationId: location_id,
-      fromDateTime: from_date_time,
-      includeEmployees: true
-    }}
-    let(:response) { 'resp' }
-
-    before do
-      expect(client).to receive(:get)
-                          .with('/v5/availability/1day', expected_params, Booker::V5::Models::AvailabilityResult)
-                          .and_return(response)
-    end
-
-    it 'delegates to get and returns' do
-      expect(result).to be response
-    end
-  end
-
   describe '#two_day' do
     let(:location_id) { 456 }
     let(:from_date_time) { 'from_date_time' }
