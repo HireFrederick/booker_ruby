5d3e8d3183d397b0bcf5bbee96021132ecfddb64
Jash Bhankharia <jashnb@gmail.com>
2020-09-10 15:32:05 -0400

    add address attribute to employee model

--- lib/booker/version.rb
+++ lib/booker/version.rb
@@ -1,3 +1,3 @@
 module Booker
-  VERSION = '3.3.17'
+  VERSION = '3.3.18'
 end
--- spec/lib/booker/v4/models/employee_spec.rb
+++ spec/lib/booker/v4/models/employee_spec.rb
@@ -4,7 +4,7 @@ describe Booker::V4::Models::Employee do
   it { is_expected.to be_a Booker::V4::Models::Model }
 
   it 'has the correct attributes' do
-    ['ID', 'FirstName', 'LastName', 'Gender', 'MobilePhone'].each do |attr|
+    %w[ID FirstName LastName Gender MobilePhone Address].each do |attr|
       expect(subject).to respond_to(attr)
     end
   end
